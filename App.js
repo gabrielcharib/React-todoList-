c6f486d0823d073f9c2e5a9139edb527b145567d
Gabriel Charib <charib1997@outlook.com>
2021-10-06 22:05:50 +0200

    add componets Task formatting task components

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      {/*today task */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitre } >Today's task</Text>
+
+        <View style={styles.items}>
+        {/* the task will go !  */}
+        <Task text={'Task 1 '}>   </Task>
+        <Task text={'Task 2 '}>   </Task>
+        </View>
+
+      </View>
+
+
+
+     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED', 
+  },
+  taskWrapper:{
+    paddingTop:80,
+    paddingHorizontal:20,
+  },
+  sectionTitre: {
+    fontSize: 24,
+    fontWeight: 'bold'
+
+  },
+  items: {
+     marginTop: 30,
   },
 });
